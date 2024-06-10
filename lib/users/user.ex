aa4de2dec9bf477f51b461ca1faa865d4ca6ca8b
Andre Cristiano <andre.cristiano1975@gmail.com>
2024-06-10 10:44:30 -0300

    feat: create the User, Booking strucs and theres builds

--- lib/users/user.ex
+++ lib/users/user.ex
@@ -1,9 +1,21 @@
 defmodule Flightex.Users.User do
+  alias Flightex.Users.User
+
   @keys [:name, :email, :cpf, :id]
   @enforce_keys @keys
   defstruct @keys
 
-  def build do
-    # TO DO
+  def build(name, email, cpf) when is_bitstring(cpf) do
+    id = UUID.uuid4()
+
+    {:ok,
+     %User{
+       id: id,
+       name: name,
+       email: email,
+       cpf: cpf
+     }}
   end
+
+  def build(_name, _email, _cpf), do: {:error, "Cpf must be a String"}
 end
